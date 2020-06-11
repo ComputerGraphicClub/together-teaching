bd1e329c1ead606bb76a5c7b264ef77e1760d48a
romain Talou <talou@admins-MacBook-Pro-235.local>
2020-06-11 12:08:40 +0200

    Add port from environement variable

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 
-let express = require('express');
-let app = express();
-let host = 3000
-let server = app.listen(host)
+const express = require('express');
+const app = express();
+const port = process.env.PORT  || 3000 ;
+const server = app.listen(port)
 
 app.use(express.static('public'));
 
-console.log("Socket server is running. localhost:" + host)
+console.log("Socket server is running. localhost:" + port)
 
-let socket = require('socket.io')
-let io = socket(server);
+const socket = require('socket.io')
+const io = socket(server);
 
 io.sockets.on('connection', newConnection)
 
 function newConnection(socket){
 	console.log('connection:',	socket.id);
 	socket.on('mouse', mouseMsg);
-	
+
 	function mouseMsg(data) {
 		socket.broadcast.emit('mouse', data)
 		console.log(data)
