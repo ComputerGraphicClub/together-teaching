e83ffd963973854cf815843ed3c66786a8d2db69
romain Talou <talou@tsf-452-wpa-4-096.epfl.ch>
2020-09-06 09:47:30 +0200

    ipad-COLORl

--- public/scripts/sketchRythm.js
+++ public/scripts/sketchRythm.js
@@ -12,11 +12,12 @@ let bgColor = 0;
 let bg;
 let stateDrum = 0;
 let stateClap = 0;
+
 function setup() {
 
   bg = loadImage('img/bg_cardboard.jpg');
-	socket = io.connect('https://together-teaching.herokuapp.com/')
-	//socket = io.connect('http://localhost:3000')
+	//socket = io.connect('https://together-teaching.herokuapp.com/')
+	socket = io.connect('http://localhost:3000')
   socket.on('mouse', newDrawing);
 	socket.on('mouse', newSound);
   createCanvas(windowWidth, windowHeight);
@@ -30,7 +31,8 @@ function setup() {
 	//frameRate(10);
 
 
-  clr = random(360);
+  //clr = random(360);
+  clr = 50;
   //noStroke()
 
   pg = createGraphics(windowWidth, windowHeight);
@@ -55,6 +57,84 @@ function setup() {
 	platform2 = createSprite((windowWidth/4)*3, (windowHeight/2)+100);
   platform2.addAnimation('normal', 'img/small_platform0001.png', 'img/small_platform0003.png');
 
+  //Setup UI
+
+  myButton_color_red = new Clickable();     //Create button
+  myButton_color_green = new Clickable();     //Create button
+  myButton_color_erase = new Clickable();     //Create Button
+  myButton_color_black = new Clickable();     //Create button
+
+
+  myButton_color_red.locate((windowWidth-80), 50);
+  myButton_color_red.width = 30;
+  myButton_color_red.height = 30;
+  myButton_color_red.strokeWeight = 1;
+  myButton_color_red.cornerRadius = 30;
+  myButton_color_red.text = "";
+  myButton_color_red.stroke = "#FEFFCD";
+  myButton_color_red.color = "#FE2C02";   //Position Button
+  myButton_color_red.onPress = function(){  //When myButton is pressed
+    this.strokeWeight = 3;
+    myButton_color_green.strokeWeight = 1;
+    myButton_color_erase.strokeWeight = 1;
+    myButton_color_black.strokeWeight = 1;    //Change button color
+    //bg= 0;
+    clr = 10;            //Show an alert message
+  }
+
+  myButton_color_green.locate((windowWidth-80), 90);
+  myButton_color_green.width = 30;
+  myButton_color_green.height = 30;
+  myButton_color_green.strokeWeight = 1;
+  myButton_color_green.cornerRadius = 30;
+  myButton_color_green.text = "";
+  myButton_color_green.stroke = "#FEFFCD";
+  myButton_color_green.color = "#00FFBE";   //Position Button
+  myButton_color_green.onPress = function(){  //When myButton is pressed
+    this.strokeWeight = 3;
+    myButton_color_red.strokeWeight = 1;
+    myButton_color_erase.strokeWeight = 1;
+    myButton_color_black.strokeWeight = 1;     //Change button color
+    //bg= 0;
+    clr = 165;            //Show an alert message
+  }
+
+  myButton_color_black.locate((windowWidth-80), 130);
+  myButton_color_black.width = 30;
+  myButton_color_black.height = 30;
+  myButton_color_black.strokeWeight = 1;
+  myButton_color_black.cornerRadius = 30;
+  myButton_color_black.text = "";
+  myButton_color_black.stroke = "#FEFFCD";
+  myButton_color_black.color = "#000000";   //Position Button
+  myButton_color_black.onPress = function(){  //When myButton is pressed
+    this.strokeWeight = 3;
+    myButton_color_red.strokeWeight = 1;
+    myButton_color_green.strokeWeight = 1;
+    myButton_color_erase.strokeWeight = 1;  //Change button color
+    //bg= 0;
+    clr = 190;            //Show an alert message
+  }
+
+  myButton_color_erase.locate((windowWidth-80), 170);
+  myButton_color_erase.width = 30;
+  myButton_color_erase.height = 30;
+  myButton_color_erase.strokeWeight = 1;
+  myButton_color_erase.cornerRadius = 30;
+  myButton_color_erase.text = "";
+  myButton_color_erase.stroke = "#FEFFCD";
+  myButton_color_erase.color = "#B0864E";   //Position Button
+  myButton_color_erase.onPress = function(){  //When myButton is pressed
+    this.strokeWeight = 3;
+    myButton_color_red.strokeWeight = 1;
+    myButton_color_green.strokeWeight = 1;
+    myButton_color_black.strokeWeight = 1;      //Change button color
+    //bg= 0;
+    clr = 240;            //Show an alert message
+  }
+
+
+
 }
 
 
@@ -63,7 +143,7 @@ function setup() {
 function displayDot(x, y, color, color2 = 100){
 	drawingCanvas.colorMode(HSB)
 	drawingCanvas.fill(color, 100, color2)
-	drawingCanvas.ellipse(x, y, 15)
+	drawingCanvas.ellipse(x, y, 10)
 	drawingCanvas.colorMode(RGB)
 }
 
@@ -82,6 +162,7 @@ else if (stateClap === 1) {
 	asterisk2.velocity.y = -JUMP;
 }
 
+
 }
 
 
@@ -94,6 +175,15 @@ function draw() {
 
 
  image(drawingCanvas, 0, 0);
+
+// UI
+
+ myButton_color_red.draw();
+ myButton_color_green.draw();
+ myButton_color_black.draw();
+ myButton_color_erase.draw();
+
+ // Fin UI
 
  stroke(255, 255, 204);
  strokeWeight(3);
@@ -179,7 +269,9 @@ stroke(0, 255, 255);
 
 
 
+
 }
+
 
 
 
@@ -207,7 +299,7 @@ else {}
 function keyPressed() {
 	if (keyCode === 65) {
     stateDrum = 1;
-
+    bg=0;
 }
 
 else if (keyCode === 90) {
@@ -225,9 +317,9 @@ function mousePressed(){
 	mouseDragged()
 }
 function mouseDragged() {
-	clr += 1
-	clr = upgradeColor(clr)
-
+	// clr += 1
+	// clr = upgradeColor(clr)
+  //clr = 10;  //orange sanguine
 
 
 	let data = {
